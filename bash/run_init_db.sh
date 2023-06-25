a212899bea1227dd9593738e0c9c52401a59a33c
bulatstoff <111246450+bulatstoff@users.noreply.github.com>
2023-06-25 19:38:54 +0300

    ура

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+docker run --name sqldocker -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER=test_sde -e POSTGRES_DB=demo -d -p 5432:5432 -v C:\Users\BKhabibullin\Documents\GitHub\sde_test_db/sql:/var/lib/postgresql/sql postgres
+sleep 5
+docker exec -it sqldocker psql -d demo -U test_sde -f /var/lib/postgresql/sql/init_db/demo.sql
